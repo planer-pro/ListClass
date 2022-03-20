4cbed551b78841e2edbb441257f92707fdd07489
Planer <planer.reg@gmail.com>
2022-03-20 14:26:06 +0300

    initial_commit

---
+++ list.cpp
@@ -0,0 +1,20 @@
+class List
+{
+public:
+  byte length;
+  byte data[256];
+
+  void append(byte item)
+  {
+    if (length < 256)
+      data[length++] = item;
+  }
+
+  void remove(byte index)
+  {
+    if (index >= length)
+      return;
+    memmove(&data[index], &data[index + 1], length - index - 1);
+    length--;
+  }
+};
